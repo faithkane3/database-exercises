60d0d7d5050002e5e6a55b5862d7199a6b5c2d7a
faithkane3 <faith.x.kane@gmail.com>
2019-09-03 15:35:31 -0500

    Committing 3.3_tables_exercises.sql

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,50 @@
+-- 3.3_tables_exercises.sql
+
+-- shows tables and views to the left like the dropdown menu
+use employees;
+
+-- lists al the tables in the database 
+show tables;
+
+-- shows the different data types present in this table
+show create table employees;
+
+-- CREATE TABLE `employees` (
+  `emp_no` int(11) NOT NULL,
+  `birth_date` date NOT NULL,
+  `first_name` varchar(14) NOT NULL,
+  `last_name` varchar(16) NOT NULL,
+  `gender` enum('M','F') NOT NULL,
+  `hire_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+-- Numeric Type in tables
+show create table departments;
+describe departments;
+
+-- String Type in tables
+describe dept_emp;
+
+-- Date Type in tables  
+describe current_dept_emp;
+
+-- Relationship between employees and departments tables? I don't see a column in common.
+-- CREATE TABLE `departments` (
+  `dept_no` char(4) NOT NULL,
+  `dept_name` varchar(40) NOT NULL,
+  PRIMARY KEY (`dept_no`),
+  UNIQUE KEY `dept_name` (`dept_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+-- show table that created the dept_manager table
+show create table dept_manager;
+-- CREATE TABLE `dept_manager` (
+  `emp_no` int(11) NOT NULL,
+  `dept_no` char(4) NOT NULL,
+  `from_date` date NOT NULL,
+  `to_date` date NOT NULL,
+  PRIMARY KEY (`emp_no`,`dept_no`),
+  KEY `dept_no` (`dept_no`),
+  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,
+  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1

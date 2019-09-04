8a4c669148008627a8108788da3652de9b5c6bcc
faithkane3 <faith.x.kane@gmail.com>
2019-09-04 15:58:12 -0500

    commit exercise 3.7_group_by_exercise.sql

---
+++ 3.7_group_by_exercises.sql
@@ -0,0 +1,47 @@
+3.7_group_by_exercises.sql
+
+describe titles;
+-- 2. In your script, use DISTINCT to find the unique titles in the titles table. 
+SELECT DISTINCT title
+FROM titles;
+-- 3. Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY. 
+use employees;
+describe employees;
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE'E%E'
+GROUP BY last_name; 
+-- 4. Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+SELECT DISTINCT last_name, first_name
+FROM employees
+WHERE last_name LIKE'E%E'
+GROUP BY last_name, first_name;
+-- 5. Find the unique last names with a 'q' but not 'qu'.
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE'%q%'
+AND last_name NOT LIKE'%qu%';
+-- 6. Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+SELECT DISTINCT last_name, COUNT(*) AS number
+FROM employees
+WHERE last_name LIKE'%q%'
+AND last_name NOT LIKE'%qu%'
+GROUP BY last_name
+ORDER BY last_name;
+-- 7. Update your query for 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
+SELECT first_name, COUNT(*) AS number, gender
+FROM employees
+WHERE first_name IN('Irena', 'Vidya', 'Maya')
+GROUP BY first_name, gender;
+-- 8. Recall the query the generated usernames for the employees from the last lesson. Are there any duplicate usernames?  Yes 
+SELECT DISTINCT LOWER(
+			CONCAT(
+				SUBSTR(first_name, 1, 1), 
+				SUBSTR(last_name, 1, 4), 
+				'_', 
+				DATE_FORMAT(birth_date,'%m%y')
+				)
+			) 
+			AS username, first_name, last_name, birth_date
+FROM employees;
+-- BONUS -- How many duplicate usernames are there?

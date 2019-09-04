cc296e75990f8cb3487c6f65cd70cb324ab0b617
faithkane3 <faith.x.kane@gmail.com>
2019-09-04 10:51:43 -0500

    corrections made to remove () from LIKE expressions

--- 3.5.1_where_exercises.sql
+++ 3.5.1_where_exercises.sql
@@ -7,7 +7,7 @@ SELECT * FROM employees
 WHERE first_name IN('Irena', 'Vidya', 'Maya');
 -- select all employees with last names starting with Even
 SELECT * FROM employees
-WHERE last_name LIKE('E%');
+WHERE last_name LIKE'E%';
 -- select all employees hired in the 90s  135,214
 SELECT * FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
@@ -23,14 +23,14 @@ WHERE (first_name='Irena' OR first_name='Vidya' OR first_name='Maya')
 AND gender='M';
 -- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
 SELECT * FROM employees
-WHERE last_name LIKE('E%') OR last_name LIKE('%E');
+WHERE last_name LIKE'E%' OR last_name LIKE'%E';
 --  Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
 SELECT * FROM employees
-WHERE last_name LIKE('E%') AND last_name LIKE('%E');
+WHERE last_name LIKE'E%E';
 -- Find all employees hired in the 90s and born on Christmas — 362 rows.
 SELECT * FROM employees
-WHERE (hire_date BETWEEN '90-01-01' AND '99-12-31')
-AND birth_date LIKE('%-12-25');
+WHERE hire_date BETWEEN '90-01-01' AND '99-12-31'
+AND birth_date LIKE'%-12-25';
 -- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
 SELECT * FROM employees
-WHERE last_name LIKE('%q%') AND last_name NOT LIKE('%qu%');
+WHERE last_name LIKE'%q%' AND last_name NOT LIKE'%qu%';

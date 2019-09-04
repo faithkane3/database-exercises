0c67b1bce6ab4e4cf869cf8701171cf127ddb985
faithkane3 <faith.x.kane@gmail.com>
2019-09-04 10:03:20 -0500

    completed exercise

--- 3.5.1_where_exercises.sql
+++ 3.5.1_where_exercises.sql
@@ -19,4 +19,18 @@ SELECT * FROM employees
 WHERE last_name LIKE '%q%';
 -- use or instead of in for number 2
 SELECT * FROM employees
-WHERE first_name 'Irena' OR 'Vidya' OR 'Maya';
+WHERE (first_name='Irena' OR first_name='Vidya' OR first_name='Maya')
+AND gender='M';
+-- Find all employees whose last name starts or ends with 'E' — 30,723 rows.
+SELECT * FROM employees
+WHERE last_name LIKE('E%') OR last_name LIKE('%E');
+--  Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT * FROM employees
+WHERE last_name LIKE('E%') AND last_name LIKE('%E');
+-- Find all employees hired in the 90s and born on Christmas — 362 rows.
+SELECT * FROM employees
+WHERE (hire_date BETWEEN '90-01-01' AND '99-12-31')
+AND birth_date LIKE('%-12-25');
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT * FROM employees
+WHERE last_name LIKE('%q%') AND last_name NOT LIKE('%qu%');

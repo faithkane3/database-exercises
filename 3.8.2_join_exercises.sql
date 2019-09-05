2db8085dcbf6746caea6038e9d45fa648c1a0852
faithkane3 <faith.x.kane@gmail.com>
2019-09-05 12:08:44 -0500

    Commit updated #2

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -1,10 +1,11 @@
 3.8.2_join_exercises.sql
-
+-- Join Example Database
 -- 1. Use the join_example_db. Select all the records from both the users and roles tables.
 use join_example_db;
 SELECT * FROM users;
 SELECT * FROM roles;
--- 2. Use join, left join, and right join to combine results from the users and roles tables as we did in the lesson. Before you run each query, guess the expected number of results.
+-- 2. Use join, left join, and right join to combine results from the users and roles tables as we did in the lesson. 
+-- Before you run each query, guess the expected number of results.
 -- join
 SELECT users.id, users.name, users.email, roles.name as roles 
 FROM users
@@ -20,9 +21,21 @@ SELECT users.id, users.name, users.email, roles.name as roles
 FROM users
 RIGHT JOIN roles
 ON users.id=roles.id;
--- 3. Use count and the appropriate join type to get a list of roles along with the number of users that has the role. Hint: You will also need to use group by in the query.
+-- 3. Use count and the appropriate join type to get a list of roles along with the number of users that has the role. 
+-- Hint: You will also need to use group by in the query.
 SELECT roles.name as roles, count(users.id) as employee_count
 FROM users
 LEFT JOIN roles
 ON users.role_id=roles.id
 GROUP BY roles;
+-- Employees Database
+use employees;
+-- 2. Using the example in the Associative Table Joins section as a guide, 
+-- write a query that shows each department along with the name of the current manager for that department.
+SELECT DISTINCT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees AS e
+JOIN dept_manager AS dm
+ON dm.emp_no=e.emp_no
+JOIN departments AS d
+ON d.dept_no=dm.dept_no
+ORDER BY dept_name;

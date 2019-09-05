87d35ad0e907f0bde84fee940cd04fdf72e59d19
faithkane3 <faith.x.kane@gmail.com>
2019-09-05 14:53:43 -0500

    Commit updated number 4

--- 3.8.2_join_exercises.sql
+++ 3.8.2_join_exercises.sql
@@ -39,3 +39,23 @@ ON dm.emp_no=e.emp_no
 JOIN departments AS d
 ON d.dept_no=dm.dept_no
 ORDER BY dept_name;
+-- 3. Find the name of all departments currently managed by women. 
+SELECT DISTINCT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees AS e
+JOIN dept_manager AS dm
+ON dm.emp_no=e.emp_no
+JOIN departments AS d
+ON d.dept_no=dm.dept_no
+WHERE e.gender='F'
+ORDER BY dept_name;
+-- 4. Find the current titles of employees currently working in the Customer Service department.
+-- describe departments;
+
+SELECT t.title, COUNT(t.title) AS Count
+FROM titles AS t
+JOIN dept_emp AS de
+ON t.emp_no=de.emp_no
+JOIN departments AS d
+ON de.dept_no=d.dept_no
+WHERE d.dept_no='d009' AND de.to_date='9999-01-01' AND t.to_date='9999-01-01'
+GROUP BY t.title;

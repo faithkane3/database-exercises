d039ce5a3656d3e3bac642b995e70d40b8cd2328
faithkane3 <faith.x.kane@gmail.com>
2019-09-04 19:58:32 -0500

    Commit exercises 3.7

--- 3.7_group_by_exercises.sql
+++ 3.7_group_by_exercises.sql
@@ -29,11 +29,12 @@ AND last_name NOT LIKE'%qu%'
 GROUP BY last_name
 ORDER BY last_name;
 -- 7. Update your query for 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
-SELECT first_name, COUNT(*) AS number, gender
+SELECT COUNT(first_name) AS number, gender
 FROM employees
 WHERE first_name IN('Irena', 'Vidya', 'Maya')
-GROUP BY first_name, gender;
--- 8. Recall the query the generated usernames for the employees from the last lesson. Are there any duplicate usernames?  Yes 
+GROUP BY gender;
+-- 8. Recall the query the generated usernames for the employees from the last lesson. Are there any duplicate usernames?  
+-- Yes. Query without DISTINCT returns 300,024 rows, and query without DISTINCT returns 285,872 rows.
 SELECT DISTINCT LOWER(
 			CONCAT(
 				SUBSTR(first_name, 1, 1), 
@@ -45,3 +46,4 @@ SELECT DISTINCT LOWER(
 			AS username, first_name, last_name, birth_date
 FROM employees;
 -- BONUS -- How many duplicate usernames are there?
+-- There are 14,152 duplicate usernames.

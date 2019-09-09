cc1706947e431ae38313adc556261c17ad1f627c
faithkane3 <faith.x.kane@gmail.com>
2019-09-09 11:30:02 -0500

    Commit answers uing world db

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -47,5 +47,62 @@ ORDER BY population DESC;
 SELECT AVG(LifeExpectancy)
 FROM country;
 
+/*What is the average life expectancy for each region, each continent? Sort the results from shortest to longest*/
+
+SELECT Continent,
+	   AVG(LifeExpectancy) AS life_expectancy
+FROM country
+GROUP BY Continent
+ORDER BY life_expectancy;
+
+/*By Region*/
+
+SELECT Region,
+	   AVG(LifeExpectancy) AS life_expectancy
+FROM country
+GROUP BY Region
+ORDER BY life_expectancy;
+
+/*Bonus
+Find all the countries whose local name is different from the official name*/
+
+SELECT Name
+FROM country
+WHERE Name<>LocalName AND LocalName<>'-';
+
+/*How many countries have a life expectancy less than x?*/
+
+SELECT COUNT(name)
+FROM country
+WHERE LifeExpectancy<500000;
+
+/*What state is city x located in?*/
+
+SELECT district
+FROM city
+WHERE name='San Antonio';
+
+/*What region of the world is city x located in?*/
+
+SELECT region
+FROM country
+JOIN city
+ON city.CountryCode=country.Code
+WHERE city.name='San Antonio';
 
 
+/*What country (use the human readable name) city x located in?*/
+
+SELECT country.name
+FROM country
+JOIN city
+ON country.code=city.countrycode
+WHERE city.name='San Antonio';
+
+/*What is the life expectancy in city x?*/
+
+SELECT lifeexpectancy
+FROM country
+JOIN city
+ON city.countrycode=country.code
+WHERE city.name='San Antonio';

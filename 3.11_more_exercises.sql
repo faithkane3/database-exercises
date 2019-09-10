150c0d28e0eb8007238507585ccf36cb25f78563
faithkane3 <faith.x.kane@gmail.com>
2019-09-09 20:09:55 -0500

    Commit answer up to Joins

--- 3.11_more_exercises.sql
+++ 3.11_more_exercises.sql
@@ -313,3 +313,311 @@ SELECT  name,
 	JOIN payment USING(rental_id)
 	GROUP BY name
 	ORDER BY gross_revenue DESC;
+/*1. SELECT statements*/
+
+/*Select all columns from the actor table.*/
+
+SELECT *
+FROM actor;
+
+/*Select only the last_name column from the actor table.*/
+
+SELECT DISTINCT last_name
+FROM actor;
+
+/*Select only the following columns from the film table.*/
+
+SELECT DISTINCT title, 
+				release_year
+FROM film;
+
+/*2. DISTINCT operator
+
+Select all distinct (different) last names from the actor table.*/
+
+SELECT DISTINCT last_name
+FROM actor;
+
+
+/*Select all distinct (different) postal codes from the address table.*/
+
+SELECT DISTINCT postal_code
+FROM address;
+
+/*Select all distinct (different) ratings from the film table.*/
+
+SELECT DISTINCT rating
+FROM film;
+
+/*WHERE clause
+
+Select the title, description, rating, movie length columns from the films table 
+that last 3 hours or longer.*/
+
+SELECT title,
+	   description,
+	   rating,
+	   length
+	FROM film
+	WHERE length>=180;
+
+/*Select the payment id, amount, and payment date columns from the payments table 
+for payments made on or after 05/27/2005.*/
+
+SELECT payment_id,
+	   amount,
+	   payment_date
+	FROM payment
+	WHERE payment_date>"2005-05-26";
+
+/*Select the primary key, amount, and payment date columns from the payment table 
+for payments made on 05/27/2005.*/
+
+SELECT payment_id,
+	   amount,
+	   payment_date
+	FROM payment
+	WHERE payment_date>"2005-05-26";
+
+/*Select all columns from the customer table 
+for rows that have a last names beginning with S and a first names ending with N.*/
+
+SELECT *
+	FROM customer
+	WHERE last_name LIKE 'S%' AND first_name LIKE '%N';
+
+/*Select all columns from the customer table 
+for rows where the customer is inactive or has a last name beginning with "M".*/
+
+
+SELECT *
+	FROM customer
+	WHERE last_name LIKE 'M%' AND active=0;
+
+
+/*Select all columns from the category table for rows where 
+the primary key is greater than 4 and the name field begins with either C, S or T.*/
+
+SELECT *
+	FROM category
+	WHERE category_id>4 AND name LIKE 'C%' OR name LIKE 'S%' OR name LIKE 'T%';
+
+/*Select all columns minus the password column from the staff table 
+for rows that contain a password.*/
+
+SELECT staff_id,
+	   last_name,
+	   first_name,
+	   address_id,
+	   picture,
+	   email,
+	   store_id,
+	   active,
+	   username,
+	   last_update
+	FROM staff
+	WHERE password IS NOT NULL;
+
+/*Select all columns minus the password column from the staff table f
+or rows that do not contain a password.*/
+
+SELECT staff_id,
+	   last_name,
+	   first_name,
+	   address_id,
+	   picture,
+	   email,
+	   store_id,
+	   active,
+	   username,
+	   last_update
+	FROM staff
+	WHERE password IS Null;
+
+/*IN operator
+
+Select the phone and district columns from the address table 
+for addresses in California, England, Taipei, or West Java.*/
+
+SELECT phone,
+	   district
+	FROM address
+	WHERE district IN ('California', 'England', 'Taipei', 'West Java');
+
+/*Select the payment id, amount, and payment date columns from the payment table 
+for payments made on 05/25/2005, 05/27/2005, and 05/29/2005. 
+(Use the IN operator and the DATE function, instead of the AND operator as in previous exercises.)*/
+
+SELECT payment_id,
+	   amount,
+	   payment_date
+	FROM payment
+	WHERE DATE(payment_date) IN ('2005-05-25', '2005-05-27', '2005-05-29');
+
+/*Select all columns from the film table for films rated G, PG-13 or NC-17.*/
+
+SELECT *
+	FROM film
+	WHERE rating IN ('G', 'PG-13', 'NC-17');
+
+/*BETWEEN operator
+
+Select all columns from the payment table for payments 
+made between midnight 05/25/2005 and 1 second before midnight 05/26/2005.*/
+
+SELECT *
+	FROM payment
+	WHERE DATE(payment_date) BETWEEN '2005-05-25' AND '2005-05-26 23:59:59';
+
+/*Select the following columns from the film table for films where the length of the description 
+is between 100 and 120.*/
+
+SELECT title,
+	   rental_duration * rental_rate AS total_rental_cost
+	FROM film
+	WHERE CHAR_LENGTH(description) BETWEEN 100 AND 120;
+
+/*LIKE operator
+
+Select the following columns from the film table 
+for rows where the description begins with "A Thoughtful".*/
+
+SELECT *
+	FROM film
+	WHERE description LIKE 'A Thoughtful%';
+
+/*Select the following columns from the film table 
+for rows where the description ends with the word "Boat".*/
+
+SELECT *
+	FROM film
+	WHERE description LIKE '%Boat';
+
+/*Select the following columns from the film table 
+where the description contains the word "Database" 
+and the length of the film is greater than 3 hours.*/
+
+SELECT *
+	FROM film
+	WHERE description LIKE '%Database%';
+
+/*LIMIT Operator
+
+Select all columns from the payment table and only include the first 20 rows.*/
+
+SELECT *
+	FROM payment
+	LIMIT 20;
+
+/*Select the payment date and amount columns from the payment table 
+for rows where the payment amount is greater than 5, 
+and only select rows whose zero-based index in the result set is between 1000-2000.*/
+
+SELECT payment_date,
+	   amount
+	FROM payment
+	WHERE amount > 5
+	LIMIT 1001
+	OFFSET 999;
+
+/*Select all columns from the customer table, l
+imiting results to those where the zero-based index is between 101-200.*/
+
+SELECT *
+	FROM customer
+	LIMIT 99
+	OFFSET 100;
+
+/*ORDER BY statement
+
+Select all columns from the film table and order rows by the length field 
+in ascending order.*/
+
+SELECT *
+	FROM film
+	ORDER BY length;
+
+/*Select all distinct ratings from the film table ordered by rating 
+in descending order.*/
+
+SELECT DISTINCT rating
+	FROM film
+	ORDER BY rating DESC;
+
+/*Select the payment date and amount columns from the payment table 
+for the first 20 payments ordered by payment amount in descending order.*/
+
+SELECT payment_date,
+	   amount
+	FROM payment
+	ORDER BY amount DESC
+	LIMIT 20;
+
+/*Select the title, description, special features, length, and rental duration columns 
+from the film table for the first 10 films with behind the scenes footage 
+under 2 hours in length and a rental duration between 5 and 7 days, 
+ordered by length in descending order.*/
+
+SELECT title,
+	   description,
+	   special_features,
+	   length,
+	   rental_duration
+	FROM film
+		WHERE special_features LIKE '%Behind the Scenes%' 
+			AND length<120 
+				AND rental_duration BETWEEN 5 AND 7
+		ORDER BY length DESC
+		LIMIT 10;
+
+/*JOINs
+
+Select customer first_name/last_name and actor first_name/last_name columns 
+from performing a left join between the customer and actor column 
+on the last_name column in each table. (i.e. customer.last_name = actor.last_name)
+
+Label customer first_name/last_name columns as customer_first_name/customer_last_name
+
+Label actor first_name/last_name columns in a similar fashion.
+returns correct number of records: 599*/
+
+
+
+/*Select the customer first_name/last_name and actor first_name/last_name columns 
+from performing a /right join between the customer and actor column 
+on the last_name column in each table. (i.e. customer.last_name = actor.last_name)
+
+returns correct number of records: 200*/
+
+/*Select the customer first_name/last_name and actor first_name/last_name columns 
+from performing an inner join between the customer and actor column 
+on the last_name column in each table. (i.e. customer.last_name = actor.last_name)
+
+returns correct number of records: 43*/
+
+
+
+/*Select the city name and country name columns from the city table, 
+performing a left join with the country table to get the country name column.
+
+Returns correct records: 600*/
+
+
+
+/*Select the title, description, release year, and language name columns 
+from the film table, performing a left join with the language table 
+to get the "language" column.
+
+Label the language.name column as "language"
+
+Returns 1000 rows*/
+
+
+
+/*Select the first_name, last_name, address, address2, city name, district, 
+and postal code columns from the staff table, performing 2 left joins 
+with the address table then the city table to get the address and city related 
+columns.
+
+returns correct number of rows: 2*/
+
